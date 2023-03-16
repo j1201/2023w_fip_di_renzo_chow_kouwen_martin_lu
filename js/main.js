b262ca590517e0843ddf02379c9a40a33da2be4f
zeyinlu <99000729+zeyinlu@users.noreply.github.com>
2023-03-16 15:17:43 -0400

    Revert "Dev.zl.video camp"

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
-import TheManiPara from "./components/ManiPara.js";
-
 (() => {
     const { createApp } = Vue
 
     createApp({
         created() {
+            // get your remote data here 
+            //fetch('./data/data.json')
+            fetch('./scripts/json.php')
+                .then(res => res.json()) 
+                .then(data => this.petitionData = data) 
+            .catch(error => {
+                console.error(error);
+            });
+        },
+        
+        mounted() {
             window.addEventListener('scroll', this.handleScroll);
         },
-
-        mounted() {
-            
-        },
-
+        
         data() {
             return {
-                showLoader: setTimeout(this.hideLoader, 2000),
+               // showLoader: setTimeout(this.hideLoader, 2000),
                 sideBar: false,
-                isActive: false,
-                paras: [1,2,3]
+                currentSection: '',
+                sections: [],
+                petitionData: {}
+                
             }
         },
 
@@ -28,30 +35,18 @@ import TheManiPara from "./components/ManiPara.js";
             },
 
             handleScroll() {
-                if(window.location.href = "#showMenu") {
-                   this.sideBar = true;
+                if (window.pageYOffset >= document.querySelector('#showMenu').offsetTop) {
+                  this.sideBar = true;
+                } else {
+                  this.sideBar = false;
                 }
-            },
+              }
 
             // hideLoader() {
             //     document.querySelector("#preloader").style.display = "none";
             // },
-            openCarousal() {
-                //this.isActive = !this.isActive
-                document.querySelector('#winpirnt').classList.toggle('campaignOpen')
-            },
-            openVideo() {
-                //this.isActive = !this.isActive
-                document.querySelector('#minidoc').classList.toggle('campaignOpen')
-            },
 
-        },
-
-        components: {
-            manip: TheManiPara
         }
-
-
     }).mount('#app')
 
 })()
