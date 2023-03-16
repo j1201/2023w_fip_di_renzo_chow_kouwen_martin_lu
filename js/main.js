c2db06af4f5bbfcc461f401b01523fb3d618fab8
Joanna C <joannachowhy@gmail.com>
2023-03-16 16:34:41 -0400

    conflict resolved?

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
-import TheManiPara from "./components/ManiPara.js";
-
 (() => {
     const { createApp } = Vue
 
     createApp({
         created() {
-        //     window.addEventListener('scroll', this.handleScroll);
-        // },
-
-        // mounted() {
-            
+            // get your remote data here 
+            //fetch('./data/data.json')
+            fetch('./scripts/json.php')
+                .then(res => res.json()) 
+                .then(data => this.petitionData = data) 
+            .catch(error => {
+                console.error(error);
+            });
         },
-
+        
+        mounted() {
+            window.addEventListener('scroll', this.handleScroll);
+        },
+        
         data() {
             return {
+               // showLoader: setTimeout(this.hideLoader, 2000),
                 sideBar: false,
                 currentSection: '',
                 sections: [],
                 petitionData: {}
-                isActive: false,
-                paras: [1,2,3]
-
+                
             }
         },
 
@@ -31,28 +35,18 @@ import TheManiPara from "./components/ManiPara.js";
             },
 
             handleScroll() {
-                if(window.location.href = "#showMenu") {
-                   this.sideBar = true;
+                if (window.pageYOffset >= document.querySelector('#showMenu').offsetTop) {
+                  this.sideBar = true;
+                } else {
+                  this.sideBar = false;
                 }
-            },
+              }
 
+            // hideLoader() {
+            //     document.querySelector("#preloader").style.display = "none";
+            // },
 
-            openCarousal() {
-                //this.isActive = !this.isActive
-                document.querySelector('#winpirnt').classList.toggle('campaignOpen')
-            },
-            openVideo() {
-                //this.isActive = !this.isActive
-                document.querySelector('#minidoc').classList.toggle('campaignOpen')
-            },
-
-        },
-
-        components: {
-            manip: TheManiPara
         }
-
-
     }).mount('#app')
 
 })()
