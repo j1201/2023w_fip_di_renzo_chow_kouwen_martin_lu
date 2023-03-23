2c522e409aab42c84416d6561271f42b0e5dba2e
Chimichangg <z_lu157945@fanshaweonline.ca>
2023-03-23 01:38:29 -0400

    updating more things

--- js/components/CampaignCom.js
+++ js/components/CampaignCom.js
@@ -6,20 +6,20 @@ export default {
     <div class="campaignBlock" :id='"camp"+block.id'>
     <!-- <div class="campaignContain" @click="openCarousalCom" :style="'background-image: url(/images/' + block.campBG + ')'"> -->
     <div class="campaignContain" @click="openCarousalCom">
-        <div class="campaBG">
+        <div class="campaBG" :style="'background-image: url(/images/' + block.campBG + ')'">
         </div>
         <div class="campaignHeading">
         <h3>{{block.name}}</h3>
             <p>{{block.desc}}</p>
         </div>
     </div>
-    <div id="carouselExampleIndicators" class="carousel collapse slide " data-ride="carousel">
+    <div :id='"carouselExampleIndicators"+block.id' class="carousel collapse slide " data-ride="carousel">
         <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+          <li :data-target='"#carouselExampleIndicators"+block.id' data-slide-to="0" class="active"></li>
+          <li :data-target='"#carouselExampleIndicators"+block.id' data-slide-to="1"></li>
+          <li :data-target='"#carouselExampleIndicators"+block.id' data-slide-to="3"></li>
+          <li :data-target='"#carouselExampleIndicators"+block.id' data-slide-to="4"></li>
+          <li :data-target='"#carouselExampleIndicators"+block.id' data-slide-to="5"></li>
         </ol>
         <div class="carousel-inner">
           <div class="carousel-item active carouselOver">
@@ -39,11 +39,11 @@ export default {
           </div>
 
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <a class="carousel-control-prev" :href='"#carouselExampleIndicators"+block.id' role="button" data-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <a class="carousel-control-next" :href='"#carouselExampleIndicators"+block.id' role="button" data-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
